6f9aea4e9ba6141b0228fa215b1adb5237e81306
Virendra Dangi <virendradangi285@gmail.com>
2025-08-15 23:17:06 +0530

    Replace react-reveal with react-awesome-reveal for React 18

--- src/pages/TechStack/Techstack.js
+++ src/pages/TechStack/Techstack.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Techstack.css";
 // import RubberBand from "react-reveal/RubberBand";
 import Bounce from "react-reveal/Bounce";
-import Fade from "react-reveal/Fade";
+import { Fade } from 'react-awesome-reveal';
 import { TechstackList } from "../../utils/TechstackList";
 
 const Techstack = () => {
